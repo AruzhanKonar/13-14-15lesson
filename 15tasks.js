1679e78f418709a5dd8ba77adfc022a374b3abd2
Aruna Konar <arunakonar@MBP-Aruna.Home>
2024-09-23 21:03:24 +0500

    first commit

---
+++ 15tasks.js
@@ -0,0 +1,52 @@
+let num = 42;
+console.log(num);
+
+let str="Hello world!";
+console.log(str);
+
+let isTrue = true;
+console.log(isTrue);
+
+let a = 3;
+let b = 5;
+console.log(a+b);
+
+const pi = 3.14;
+console.log(pi);
+
+var name = "Aruzhan";
+console.log(name);
+
+var age = 21;
+console.log(age);
+
+let message = "Welcome to my website.";
+console.log(message);
+
+var x = 10;
+var y = 5;
+console.log(x-y);
+
+let firstName = "Aruzhan";
+let lastName = "Konarbayeva";
+console.log(firstName,lastName);
+
+let width = 10;
+let height = 15;
+let s = width*height;
+console.log(s);
+
+let greeting = "Hello";
+console.log(greeting + ", " + name);
+
+let r = 5;
+console.log(pi * r ** 2);
+
+let num1 = 10;
+let num2 = 3;
+console.log(num1/num2);
+
+let num3 = 7;
+let num4 = 2;
+console.log(num3%num4);
+
---
+++ medium.js
@@ -0,0 +1,11 @@
+let c = 20;
+let f = (9/5) * c + 32;
+let k = c + 273.5;
+console.log( "Температура в градусах Фаренгейта:" + f + " " + "Температура в градусах Кельвина:" + k);
+
+let deposit = 1000
+let rate = 0.05
+let firstYear = deposit * (1 + rate) **  1;
+let secondYear = deposit * (1 + rate) **  2;
+let thirdYear = deposit * (1 + rate) **  3;
+console.log ("Сумма вклада через год:" + firstYear + " " + "Сумма вклада через два года:" + secondYear + " " + "Сумма вклада через три года:" + thirdYear)
